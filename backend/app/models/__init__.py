6ea90c38453be58375ccf10ab3449c1c522c5e64
storhem <storhetmax@mail.com>
2026-05-08 11:40:00 +0300

    feat: add SQLAlchemy models with PostGIS geography support

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,7 @@
+from app.models.user import User
+from app.models.property import Property
+from app.models.view import View
+from app.models.rating import Rating
+from app.models.favorite import Favorite
+
+__all__ = ["User", "Property", "View", "Rating", "Favorite"]
